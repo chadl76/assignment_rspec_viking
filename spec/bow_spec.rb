3aedeb44733b198b1347d049bf9c6374ac75611d
Chad Lucas <chad.lucas.19@gmail.com>
2017-04-07 18:30:00 -0500

    add arrow test

--- spec/bow_spec.rb
+++ spec/bow_spec.rb
@@ -10,10 +10,15 @@ describe Bow do
 			expect(bow.arrows).to eq(10)
 		end
     
-    it 'should reduce arrow count by one when used' do 
-    	expect(bow.use).to eq(arrows -= 1)
+    it 'should reduce arrow count by one when used' do
+    	bow.use
+    	expect(bow.arrows).to eq(9)
+    end
+
+    it 'should throw an error if bow use arrow with zero arrows'  do
+    	bow = Bow.new(0)
+    	bow.use
+    	expect(bow.use).to raise_error("Zero arrows left")
     end 
-
-	end
-
+  end
 end
