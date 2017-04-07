fa4634cc4d97299e1c8b99c6a7f5b0b20380b8c3
Chad Lucas <chad.lucas.19@gmail.com>
2017-04-07 14:06:29 -0500

    add receive_attack test

--- spec/viking_spec.rb
+++ spec/viking_spec.rb
@@ -3,7 +3,9 @@ require 'weapons/axe'
 
 describe Viking do
 
-	let(:viking) { Viking.new("Chad", health = 100)}
+	let(:viking) { Viking.new("Chad", health = 100) }
+	let(:thor) { Viking.new("Thor", health = 100 )}
+	let(:axe) { Weapon.new("Axe") }
   
 	describe '#initialize' do
 
@@ -16,10 +18,53 @@ describe Viking do
     end
 
     it 'does not overwrite health attribute after it has been initialized' do
-      expect(viking).not_to respond_to(:health=)
+      expect(viking.health).not_to respond_to(:health=)
     end
 
+    it 'expects weapon to be initialized to nil' do 
+    	expect(viking.weapon).to eq(nil)
+    end
+  end
 
-	end
+  describe '#pick_up_weapon' do
+
+  	it 'raises error when picking up non-weapon' do
+  		expect{viking.pick_up_weapon("hello")}.to raise_error(Exception)
+  	end
+
+  	it 'should set vikings weapon' do
+  	  viking.pick_up_weapon(axe)
+  	  expect(viking.weapon).to eq(axe)
+    end
+  end
+
+  describe '#drop_weapon' do 
+
+  	it 'should set weapon to nil' do 
+  		viking.drop_weapon
+  		expect(viking.drop_weapon).to eq(nil)
+  	end
+  end
+
+  describe '#receive_attack' do
+
+  	it 'should reduce vikings health by specified ammount' do
+  		viking.receive_attack(10)
+  		expect(viking.health).to eq(90)
+  	end
+
+  	it 'should call the take_damage method' do 
+  		expect(viking).to receive(:take_damage)
+  		viking.receive_attack(10)
+  	end
+  end
+
+  describe '#attack' do
+
+  	it 'should cause another viking health to drop' do
+  	end
+  end
+
+
 end
 
